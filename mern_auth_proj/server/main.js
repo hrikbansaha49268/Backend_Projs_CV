3b962da934771db49cfecde67a13b4ce0ebb14e3
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2024-03-24 08:12:26 +0530

    Server initiated - 2

--- mern_auth_proj/server/main.js
+++ mern_auth_proj/server/main.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const app = express();
+
+app.get("/", (req, res) => {
+    res.status(200).send({ "msg": "Hello World" });
+});
+
+app.listen(8080, () => {
+    console.log(`Server started on http://localhost:${8080}`)
+});
