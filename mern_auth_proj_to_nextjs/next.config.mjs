2a872fdc0373b41f657943168fbc3889d0871039
hrikbansaha49268 <73658678+hrikbansaha49268@users.noreply.github.com>
2024-03-31 20:56:38 +0530

    IMP

--- mern_auth_proj_to_nextjs/next.config.mjs
+++ mern_auth_proj_to_nextjs/next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    env: {
-        MONGO_URI: "mongodb+srv://hrikbans:looper@cluster0.jydd6pg.mongodb.net/mern-stack-auth?retryWrites=true&w=majority&appName=Cluster0",
-        PORT: 3000,
-        TOKEN_SECRET: "Dhakuria"
-    }
-};
+const nextConfig = {};
 
 export default nextConfig;
